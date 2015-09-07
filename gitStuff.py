e749a5a69b556e1334827f2c6aff6bc1999d4f23
adam0antium <pi@raspberrypi.(none)>
2015-09-07 08:55:57 +0000

    update git info

--- gitStuff.py
+++ gitStuff.py
@@ -7,8 +7,8 @@
 #	git clone -b theBranchName --single-branch https://github.com/adam0antium/drone.git
 #NB this script will not work for other branches since the last call is to push to master
 
-#to merge a branch into the master first checkout both versions, then switch to master, then do a "git merge branch"
-#	then check "git show" then 
+#to merge a branch into the master first checkout both versions, then switch to master
+#	with "git checkout master", then do a "git merge branch",then check with "git show"
 
 from subprocess import call
 
